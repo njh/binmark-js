fc3217f88d97a29d6ac8f28241ae99e82b455c90
Nicholas Humfrey <njh@aelius.com>
2023-01-19 22:34:12 +0000

    Created first version of demo website

---
+++ src/app.js
@@ -0,0 +1,34 @@
+import binmark from 'binmark'
+
+const inputElement = document.getElementById('input')
+const outputElement = document.getElementById('output')
+const errorElement = document.getElementById('error')
+
+function updateOutput () {
+  const input = inputElement.value
+
+  try {
+    const result = binmark.parseToHex(input)
+    outputElement.innerText = result
+    errorElement.innerText = ''
+  } catch (error) {
+    console.log(error)
+    errorElement.innerText = error
+  }
+}
+
+let debounceTimer = null
+inputElement.addEventListener('input', (event) => {
+  // Cancels the setTimeout method execution
+  if (debounceTimer) {
+    clearTimeout(debounceTimer)
+  }
+
+  // Executes the func after delay time
+  debounceTimer = setTimeout(() => {
+    debounceTimer = null
+    updateOutput()
+  }, 300)
+})
+
+updateOutput()
